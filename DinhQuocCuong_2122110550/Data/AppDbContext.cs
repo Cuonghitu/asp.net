d22f11ccc7edf1375ead4e65c9b0998f1f42e0dc
Tantien2004 <Proplayer12374@gmail.com>
2025-03-29 00:01:36 +0700

    baitap

---
+++ DinhQuocCuong_2122110550/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using DinhQuocCuong_2122110550.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DinhQuocCuong_2122110550.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public AppDbContext() { } 
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ DinhQuocCuong_2122110550/Model/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace DinhQuocCuong_2122110550.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public double Price { get; set; }
+             
+    }
+}
